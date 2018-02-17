8e35b6e98fd178cc3e2ef4825c999530171a13e8
Briton Powe <britonpowe1@Britons-MBP.attlocal.net>
2018-02-17 15:51:10 -0600

    Testing valid child states

--- PythonStarter.py
+++ PythonStarter.py
@@ -1,17 +1,26 @@
-stateVector = [3,3,1]
+stateVector = [1,1,1]
 moves = [[1,0,1],[2,0,1],[0,1,1],[0,2,1],[1,1,1]]
 childState = [0,0,0]
 allStates = []
+numCannibals = 0
+numMissionaries = 0
 for element in moves:
     print("Element: ",element)
     if stateVector [2] == 1:
-        for i in range(2):
+        for i in range(3):
             childState [i] = stateVector[i] - element[i]
         print("Child: ",childState)
     elif stateVector [2] == 0:
-        for i in range(2):
-            childState = stateVector[i] + element[i]
-    allStates.append(childState[:])
+        for i in range(3):
+            childState [i] = stateVector[i] + element[i]
+        print("Child: ",childState)
+    numCannibals = childState[1]
+    numMissionaries = childState[0]
+    numCannibals2 = 3 - numCannibals
+    numMissionaries2 = 3 - numMissionaries
+    if numCannibals <= numMissionaries or (numCannibals > 0 and numMissionaries == 0):
+        if numCannibals2 <= numMissionaries2 or (numCannibals2 > 0 and numMissionaries2 == 0):
+            if (numCannibals >= 0 and numCannibals <= 3) or (numMissionaries >= 0 and numMissionaries <= 3):
+                allStates.append(childState[:])
 print("All States: ",allStates)
 
-        
