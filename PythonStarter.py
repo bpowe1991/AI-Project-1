95c6ac5fa5cb2bbd6ad1720e5ddc69bd3c114468
Briton Powe <britonpowe1@Britons-MBP.attlocal.net>
2018-02-17 13:22:39 -0600

    Creating method for child states

--- PythonStarter.py
+++ PythonStarter.py
@@ -1,3 +1,17 @@
-print("Hello!!!")
-stateVector = [3,3,0]
-print(stateVector)
+stateVector = [3,3,1]
+moves = [[1,0,1],[2,0,1],[0,1,1],[0,2,1],[1,1,1]]
+childState = [0,0,0]
+allStates = []
+for element in moves:
+    print("Element: ",element)
+    if stateVector [2] == 1:
+        for i in range(2):
+            childState [i] = stateVector[i] - element[i]
+        print("Child: ",childState)
+    elif stateVector [2] == 0:
+        for i in range(2):
+            childState = stateVector[i] + element[i]
+    allStates.append(childState[:])
+print("All States: ",allStates)
+
+        
